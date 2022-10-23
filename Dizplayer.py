209ffca7600e352a911fbbf9034aa0c08cb06d16
Isaac Martin <melchoir55@gmail.com>
2022-10-23 13:57:24 -0700

    python streamer seems to be working

--- Dizplayer.py
+++ Dizplayer.py
@@ -1,17 +1,14 @@
 import os
 
 import discord
-import nest_asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from Listener import ListenerCog
-from Music import MusicPlayerCog
+from Music import Music
 
-nest_asyncio.apply()
 load_dotenv()
 # Get the API token from the .env file.
-DISCORD_TOKEN = os.getenv("discord_token")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 print(DISCORD_TOKEN)
 
 initial_extensions = ['cogs.listener',
@@ -21,6 +18,5 @@ intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 if __name__ == "__main__":
-    bot.add_cog(MusicPlayerCog(bot))
-    bot.add_cog(ListenerCog(bot))
+    bot.add_cog(Music(bot))
     bot.run(DISCORD_TOKEN)
--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ WORKDIR /app
 COPY ./requirements.txt /app
 
 ENV PYTHONUNBUFFERED 1
-
+RUN apt-get -y update
+RUN apt-get install -y ffmpeg
 RUN pip3 install --no-cache-dir -r requirements.txt
 COPY . .
-
-CMD ["python", "Dizplayer.py"]
