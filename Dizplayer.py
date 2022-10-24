09d2fcdb9e261d527cbdff225b61f67cc4b754cb
Isaac Martin <melchoir55@gmail.com>
2022-10-23 18:58:41 -0700

    add readme and listner

--- Dizplayer.py
+++ Dizplayer.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from Music import Music
+from Listener import ListenerCog
 
 load_dotenv()
 # Get the API token from the .env file.
@@ -19,4 +20,5 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 if __name__ == "__main__":
     bot.add_cog(Music(bot))
+    bot.add_cog(ListenerCog(bot))
     bot.run(DISCORD_TOKEN)
---
+++ README.md
@@ -0,0 +1,3 @@
+#Run
+
+`DISCORD_TOKEN=<your discord bot token> docker-compose up`
